97ac9346d48618403cf6012882c5a52ab3b78917
Jason McKee <jasonmckee@live.com>
2023-02-08 17:07:23 -0600

    remove some unused items

--- src/views/Home.js
+++ src/views/Home.js
@@ -3,24 +3,17 @@ import React from "react";
 import Hero from "../components/sections/Hero";
 import About from "../components/sections/About";
 import Learn from "../components/sections/Learn";
-import Cta from "../components/sections/Cta";
 import GetInvolved from "../components/sections/GetInvolved";
 import Support from "../components/sections/Support";
 
 const Home = () => {
   return (
     <>
-      <Hero className="illustration-section-01" />
+      <Hero />
       <About topDivider />
-      <Learn
-        invertMobile
-        topDivider
-        imageFill
-        className="illustration-section-02"
-      />
+      <Learn invertMobile topDivider />
       <GetInvolved topDivider />
       <Support topDivider />
-      <Cta split />
     </>
   );
 };
