d00f2c2d5edb53ba08398292261ead1ffbce0375
Jason McKee <jasonmckee@live.com>
2023-02-08 09:33:15 -0600

    get involved section

--- src/views/Home.js
+++ src/views/Home.js
@@ -5,6 +5,7 @@ import About from "../components/sections/About";
 import Learn from "../components/sections/Learn";
 import Testimonial from "../components/sections/Testimonial";
 import Cta from "../components/sections/Cta";
+import GetInvolved from "../components/sections/GetInvolved";
 
 const Home = () => {
   return (
@@ -17,6 +18,7 @@ const Home = () => {
         imageFill
         className="illustration-section-02"
       />
+      <GetInvolved topDivider />
       <Testimonial topDivider />
       <Cta split />
     </>
