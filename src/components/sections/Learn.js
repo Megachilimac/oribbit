6bc2e5dfaf8ab1c0314f59d3348629799a197be0
Jason McKee <jasonmckee@live.com>
2023-02-08 17:25:30 -0600

    imbed videos

--- src/components/sections/Learn.js
+++ src/components/sections/Learn.js
@@ -77,8 +77,15 @@ const Learn = ({
                 <div className="text-xxs text-color-primary fw-600 tt-u mb-8"></div>
                 <h3 className="mt-0 mb-12">QSOToday September 2022</h3>
                 <p className="m-0">
-                  View the QSOToday Sept 2022 presentation on{" "}
-                  <a href="https://youtu.be/_jN4IVccIEw">YouTube</a>
+                  <iframe
+                    width="560"
+                    height="315"
+                    src="https://www.youtube.com/embed/_jN4IVccIEw"
+                    title="YouTube video player"
+                    frameborder="0"
+                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                    allowfullscreen
+                  ></iframe>
                 </p>
               </div>
             </div>
@@ -91,8 +98,15 @@ const Learn = ({
                 <div className="text-xxs text-color-primary fw-600 tt-u mb-8"></div>
                 <h3 className="mt-0 mb-12">RATPAC November 2022</h3>
                 <p className="m-0">
-                  View the RATPAC Nov 2022 presentation on{" "}
-                  <a href="https://youtu.be/TGzgIjEt9wA">YouTube</a>
+                  <iframe
+                    width="560"
+                    height="315"
+                    src="https://www.youtube.com/embed/TGzgIjEt9wA"
+                    title="YouTube video player"
+                    frameborder="0"
+                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                    allowfullscreen
+                  ></iframe>
                 </p>
               </div>
             </div>
@@ -105,8 +119,15 @@ const Learn = ({
                 <div className="text-xxs text-color-primary fw-600 tt-u mb-8"></div>
                 <h3 className="mt-0 mb-12">How It Works (with HB9BLA)</h3>
                 <p className="m-0">
-                  Understand how it works with HB9BLA presentation on{" "}
-                  <a href="https://youtu.be/ubPP48ojJ3E">YouTube</a>
+                  <iframe
+                    width="560"
+                    height="315"
+                    src="https://www.youtube.com/embed/ubPP48ojJ3E"
+                    title="YouTube video player"
+                    frameborder="0"
+                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                    allowfullscreen
+                  ></iframe>
                 </p>
               </div>
             </div>
@@ -116,10 +137,17 @@ const Learn = ({
                 data-reveal-container=".split-item"
               >
                 <div className="text-xxs text-color-primary fw-600 tt-u mb-8"></div>
-                <h3 className="mt-0 mb-12">Rattlegram Demo</h3>
+                <h3 className="mt-0 mb-12">Rattlegram Demo by Ahmet</h3>
                 <p className="m-0">
-                  Rattlegram demo and tutorial by Ahmet on{" "}
-                  <a href="https://youtu.be/0jtzA3alpuw">YouTube</a>
+                  <iframe
+                    width="560"
+                    height="315"
+                    src="https://www.youtube.com/embed/0jtzA3alpuw"
+                    title="YouTube video player"
+                    frameborder="0"
+                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                    allowfullscreen
+                  ></iframe>
                 </p>
               </div>
             </div>
